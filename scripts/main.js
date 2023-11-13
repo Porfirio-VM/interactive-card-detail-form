8a193676808503d997cd01ff9952cadc8ac2628e
Porfirio-VM <porfirio.vergara99@gmail.com>
2023-11-13 12:09:53 -0600

    code optimization

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,23 @@ const formSection = document.querySelector('.form-wrapper')
 const form = document.getElementById('cardForm');
 const continuetoForm = document.getElementById('continue');
 
+const defaultValues = { //Defines the defaults values in the tags
+    nameTag: "Jane Appleseed",
+    cardNumberTag: "0000000000000000",
+    monthTag: "00",
+    yearTag: "00",
+    cvcTag: "000"
+}
+
+
+const inputElements = { //Defines the id of the input and then the tag it refers to
+    name: 'nameTag',
+    number: 'cardNumberTag',
+    month: 'monthTag',
+    year: 'yearTag',
+    cvc: 'cvcTag'
+}
+
 const onInputHanlder = (input, tag) =>{
     const inputValue = input.value;
     const getTagId = tag.getAttribute('id')
